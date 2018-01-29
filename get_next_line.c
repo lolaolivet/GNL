dfcd83757862979682e3a0325b5a305441396d55
Lola OLIVET <lolivet@student.42.fr>
2018-01-29 20:44:31 +0100

    line null if return 0

--- get_next_line.c
+++ get_next_line.c
@@ -5,8 +5,8 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: lolivet <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2018/01/15 15:34:43 by lolivet           #+#    #+#             */
-/*   Updated: 2018/01/26 18:18:37 by lolivet          ###   ########.fr       */
+/*   Created: 2018/01/28 14:49:14 by lolivet           #+#    #+#             */
+/*   Updated: 2018/01/29 20:43:45 by lolivet          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,25 +16,33 @@
 
 void	fill_rest(t_list *lst, char **ln, int i)
 {
-	while (((t_gnl*)(lst->content))->rest[i])
+	if (ft_strchr(REST, '\n'))
 	{
-		if (((t_gnl*)(lst->content))->rest[i] == '\n')
+		while (((t_gnl*)(lst->content))->rest[i])
 		{
-			((t_gnl*)(lst->content))->rest[i] = '\0';
-			*ln = *ln ? ft_strjoin(*ln, REST) : ft_strdup(REST);
-			break ;
+			if (((t_gnl*)(lst->content))->rest[i] == '\n')
+			{
+				((t_gnl*)(lst->content))->rest[i] = '\0';
+				*ln = *ln ? ft_strjoin(*ln, REST) : ft_strdup(REST);
+				break ;
+			}
+			i++;
 		}
-		i++;
+		REST = ft_strdup(((t_gnl*)(lst->content))->rest + i + 1);
 	}
-	REST = ft_strdup(((t_gnl*)(lst->content))->rest + i + 1);
+	else
+	{
+		*ln = ft_strdup(REST);
+		ft_strdel(&REST);
+		ft_memdel((void**)&lst);
+	}
 }
 
 int		check_rest(t_list *lst, char **line)
 {
 	if (!(lst))
 		return (0);
-	else if (((t_gnl*)(lst->content)) && ((t_gnl*)(lst->content))->rest &&
-			ft_strchr(REST, '\n'))
+	else if (((t_gnl*)(lst->content)) && REST && ft_strchr(REST, '\n'))
 	{
 		fill_rest(lst, line, 0);
 		return (2);
@@ -51,13 +59,14 @@ int		check_rest(t_list *lst, char **line)
 	}
 }
 
-int		read_file(t_list *new, char **line, int fd, int i)
+int		read_file(t_list *new, char **line, int fd, int i, int *retour)
 {
 	int				ret;
 	char			buf[BUFF_SIZE + 1];
-	
+
 	while ((ret = read(fd, buf, BUFF_SIZE)) > 0)
 	{
+		*retour = 1;	
 		buf[ret] = '\0';
 		if (ft_strchr(buf, '\n'))
 		{
@@ -74,13 +83,11 @@ int		read_file(t_list *new, char **line, int fd, int i)
 	if (ret > 0)
 	{
 		((t_gnl*)(new->content))->rest = ft_strdup(buf + i);
+		((t_gnl*)(new->content))->fd = fd;
 		return (1);
 	}
 	else
-	{
-		ft_memdel((void *)line);
-		return (0);
-	}
+			return (0);
 }
 
 int		get_next_line(const int fd, char **line)
@@ -88,23 +95,30 @@ int		get_next_line(const int fd, char **line)
 	static t_list	*lst = NULL;
 	t_list			*new;
 	int				return_check;
+	int retour = 0;
 
 	new = NULL;
 	if ((return_check = check_rest(lst, line)) == 2)
 		return (1);
-	if ((check_rest(lst, line)) == -1
-		|| !(new = ft_lstnew(ft_memalloc(sizeof(t_gnl)), sizeof(t_gnl*))))
+	if (return_check == -1
+			|| !(new = ft_lstnew(ft_memalloc(sizeof(t_gnl)), sizeof(t_gnl*))))
 		return (-1);
-	if (read_file(new, line, fd, 0))
+	if ((return_check = read_file(new, line, fd, 0, &retour)))
 	{
-		((t_gnl*)(new->content))->fd = fd;
 		ft_lstadd(&lst, new);
-	}
-	ft_memdel((void*)&new);
-	if (*line)
 		return (1);
-	else if (*line == NULL)
+	}
+//	printf("je suis le retour du retour %d\n", return_check);
+	if (return_check == 0)
+	{
+		ft_strdel(line);
 		return (0);
-	else
-		return (-1);
+	}
+	//if (*line && printf("je sors d ici\n"))
+	//	return (1);
+	//else if (*line == NULL)
+	//	return (0);
+	//else
+	//	return (-1);
+	return (0);
 }
